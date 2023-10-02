b1bbe976eb1d5297c0a9d515ea105f07d42d88b8
crystallaylalui <121093098+crystallaylalui@users.noreply.github.com>
2023-10-03 04:34:35 +0800

    fixed the back button in homescreen

--- lib/components/icons_component.dart
+++ lib/components/icons_component.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:healthy_fish_app/utils/constants/colors.dart';
 import 'package:healthy_fish_app/utils/extension/nav.dart';
 import 'package:provider/provider.dart';
-import '../seashell_provider.dart';  // Adjust the import path based on your project structure
+import '../seashell_provider.dart';
 
 class IconsComponent extends StatelessWidget {
   const IconsComponent({
@@ -11,7 +11,6 @@ class IconsComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Use Provider to get the seashell count
     int seashellCount = Provider.of<SeashellProvider>(context).seashellCount;
 
     return Row(
@@ -34,38 +33,44 @@ class IconsComponent extends StatelessWidget {
             ),
           ),
         ),
-        Container(
-          height: 44,
-          width: 44,
-          decoration: BoxDecoration(
-            color: darkBlue.withOpacity(0.8),
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Image.asset(
-                'assets/images/seashell.png',  // Correct path to your seashell icon
-                color: Colors.yellow,
-                height: 24,  // Adjust the size as needed
-                width: 24,
-              ),
-              Positioned(
-                top: -3,
-                right: -3,
-                child: Container(
-                  padding: const EdgeInsets.all(4),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: blue,
-                  ),
-                  child: Text(
-                    seashellCount.toString(),
-                    style: TextStyle(color: darkBlue),
+        GestureDetector(
+          onTap: () {
+            // Implement the logic for the second icon if needed
+            // For now, let's leave it empty
+          },
+          child: Container(
+            height: 44,
+            width: 44,
+            decoration: BoxDecoration(
+              color: darkBlue.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/seashell.png',
+                  color: Colors.yellow,
+                  height: 24,
+                  width: 24,
+                ),
+                Positioned(
+                  top: -3,
+                  right: -3,
+                  child: Container(
+                    padding: const EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: blue,
+                    ),
+                    child: Text(
+                      seashellCount.toString(),
+                      style: TextStyle(color: darkBlue),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ],
